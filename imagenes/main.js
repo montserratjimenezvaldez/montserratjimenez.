0390e1b49cc8e2ceff68e554ffc10d6273d48be3
montserratjimenezvaldez <119759202+montserratjimenezvaldez@users.noreply.github.com>
2022-12-03 15:06:34 -0600

    Add files via upload

---
+++ imagenes/main.js
@@ -0,0 +1,19 @@
+let app = document.getElementById('typewriter');
+
+let typewriter = new typewriter(app,{
+    loop: true,
+    delay: 75,
+});
+
+typewriter
+.pausefor(2500)
+.typestring('desarrollo sitios web y escribo articulos sobre programacion')
+.pausefor(200)
+.deletechars(10)
+.start();
+footer
+>2022 github, inc.
+footer Navigation 
+terms 
+privacy 
+security
